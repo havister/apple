54f037b644f5d99a5d0e70746bb4577753810a82
PointW <pointw@pointnut.com>
2019-09-25 20:42:26 +0900

    Improved players

--- players/views.py
+++ players/views.py
@@ -1,38 +1,39 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.views.generic import View, TemplateView
 from django.contrib.auth.models import User
 from trades.models import Trade
 
 
-class IndexView(LoginRequiredMixin, ListView):
-    # Closed trade list by player
-    queryset = Trade.objects.filter(
-        date_closed__isnull=False).order_by('player', '-date_closed').distinct('player')
-    context_object_name = 'player_trade_list'
+class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'players/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # Closed trade list by player
+        context['player_trade_list'] = Trade.objects.filter(
+            date_closed__isnull=False).order_by('player', '-date_closed').distinct('player')
         # New player list
         context['new_player_list'] = User.objects.filter(
             groups__name='player', trade=None).order_by('-date_joined')
         return context
 
 
-class PlayerView(LoginRequiredMixin, DetailView):
-    context_object_name = 'player'
+class PlayerView(LoginRequiredMixin, TemplateView):
     template_name = 'players/player.html'
 
-    def get_object(self):
-        # Player
-        player = get_object_or_404(User, username=self.kwargs['player_name'])
-        return player
+    def get(self, request, *args, **kwargs):
+        if request.user.username != kwargs['player_name']:
+            return redirect('players:index')
+        return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        player = context['player']
+        # Player
+        player = self.request.user
+        context['player'] = player
         # Group list
         context['group_list'] = player.groups.all()
         # Opened trade list
@@ -44,21 +45,17 @@ class PlayerView(LoginRequiredMixin, DetailView):
         return context
 
 
-class ReturnsView(LoginRequiredMixin, ListView):
-    context_object_name = 'trade_list'
+class ReturnsView(LoginRequiredMixin, TemplateView):
     template_name = 'players/returns.html'
-
-    def get_queryset(self):
-        self.player = get_object_or_404(User, username=self.kwargs['player_name'])
-        # Closed trade list
-        trade_list = Trade.objects.filter(
-            player=self.player, date_closed__isnull=False).order_by('-date_closed')
-        return trade_list
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Player
-        context['player'] = self.player
+        player = get_object_or_404(User, username=self.kwargs['player_name'])
+        context['player'] = player
         # Group list
-        context['group_list'] = self.player.groups.all()
+        context['group_list'] = player.groups.all()
+        # Closed trade list
+        context['trade_list'] = Trade.objects.filter(
+            player=player, date_closed__isnull=False).order_by('-date_closed')
         return context
